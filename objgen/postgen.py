474a8a0670ef8a655570dceb0fabf28e6eb16aae
chethan <chetandev.ksd@gmail.com>
2020-02-08 13:46:52 +0530

    param.lds linker script generation

--- objgen/postgen.py
+++ objgen/postgen.py
@@ -19,7 +19,7 @@ import objgen
 
 
 def run_postgen():
-    print('running postgen')
+
 
     #
     # POSTGEN STAGE 1: Linker support file generation
@@ -29,6 +29,25 @@ def run_postgen():
     
     param_lds = open(svar.build_path + "/param.lds", "w+")
 
-    temp = '_kernel_base_address '
+    kernel_base_address = int(sparam.kernel_sram_address) + int(device.intvec_size) * 4
+    temp = '_kernel_base_address = ' + str(kernel_base_address) + '\n'
+    param_lds.write(temp)
+
+    app_base_address = int(sparam.kernel_sram_address) + (int(sparam.kernel_section_size) * 1024)
+    temp = '_app_base_address = ' + str(app_base_address) + '\n'
+    param_lds.write(temp)
+
+    process_stack_address = int(sparam.kernel_sram_address) + (int(sparam.upper_region_size) * 1024)
+    kernel_stack_end_address = process_stack_address - 4
+    temp = '_kstack_end_addr = ' + str(kernel_stack_end_address) + '\n'
+    param_lds.write(temp)
+
+    temp = '_proc_heap_addr = ' + str(process_stack_address) + '\n'
+    param_lds.write(temp)
+
+    ghmb_address = app_base_address - (int(sparam.ghmb_region_size) * 1024)
+    temp = '_ghmb_addr = ' + str(ghmb_address) + '\n'
+    param_lds.write(temp)
+
 
     param_lds.close()
