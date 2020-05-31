4d907c5fde94b5f271d5e7e1a4a2e2a7671f9a3e
chethan <chetandev.ksd@gmail.com>
2020-05-31 22:06:11 +0530

    added base_task test program

---
+++ ossim/test/base_task.c
@@ -0,0 +1,29 @@
+/*
+ * OSSIM base task test program
+ *
+ * author: chetandev.ksd@gmail.com
+ *
+ */
+
+
+#include <stdio.h>
+#include <conio.h>
+
+#include <task.h>
+#include <init.h>
+
+
+/* 
+ * Empty user init function here
+ * base task is by defualt initialized by kernel
+ * so, in this test program there will be only base task running
+ * 
+ */
+
+uint32_t user_init() {
+
+
+    return 0;
+
+}
+
